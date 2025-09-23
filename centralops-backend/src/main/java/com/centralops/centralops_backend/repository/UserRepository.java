57e5482f54aadba5a86314a209a3da1ab110f783
Rejjiiii <regie649@gmail.com>
2025-09-24 07:07:46 +0800

    UPDATE: Commit the authentication in backend in Dev branch

--- centralops-backend/src/main/java/com/centralops/centralops_backend/repository/UserRepository.java
+++ centralops-backend/src/main/java/com/centralops/centralops_backend/repository/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.centralops.centralops_backend.model.User;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
