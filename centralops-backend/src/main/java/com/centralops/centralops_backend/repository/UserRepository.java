3f20deaadbcf4c77dcb5c144462eb988372b7581
Rejjiiii <regie649@gmail.com>
2025-09-23 12:49:42 +0800

    UPDATE: recompose the backend and setup the JSP. 100% Working

--- centralops-backend/src/main/java/com/centralops/centralops_backend/repository/UserRepository.java
+++ centralops-backend/src/main/java/com/centralops/centralops_backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.centralops.centralops_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.centralops.centralops_backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
